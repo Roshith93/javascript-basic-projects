9c44acb1468a0bcf5183402eeb161ad9d781415f
Roshith R <r.roshith@gmail.com>
2021-02-08 15:21:57 +0530

    setting random background color

--- 1-color-flipper/setup/app.js
+++ 1-color-flipper/setup/app.js
@@ -1,1 +1,12 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ['green', 'red', 'rgba(133,122,200)', '#f15025']
+const getDiv = document.querySelector('.container')
+const getVal = getDiv.querySelector('span')
+const getButton = document.querySelector('#btn')
+
+function getRandomValue(e) {
+  let random = Math.floor(Math.random() * colors.length)
+  let randomColor = colors[random]
+  getVal.textContent = randomColor
+  document.body.style.backgroundColor = randomColor
+}
+getButton.addEventListener('click', getRandomValue)
