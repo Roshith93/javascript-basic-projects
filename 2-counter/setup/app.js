ee2d744da6fe096e29f49d2d24dd0947a4cd3610
Roshith R <r.roshith@gmail.com>
2021-02-09 09:13:43 +0530

    using foreach and adding event listener

--- 2-counter/setup/app.js
+++ 2-counter/setup/app.js
@@ -8,21 +8,41 @@ let counterValue = 0
 
 const allButtons = document.querySelectorAll('.btn')
 
-decreaseButton.addEventListener('click', function (e) {
-  counterValue -= 1
-  counterSpan.textContent = counterValue
-  if (counterValue < 0) counterSpan.style.color = 'red'
-  if (counterValue === 0) counterSpan.style.color = '#222'
-})
-resetButton.addEventListener('click', function (e) {
-  counterValue = 0
-  counterSpan.textContent = counterValue
-  if (counterValue === 0) counterSpan.style.color = '#222'
+//  = using querySelectorAll
+allButtons.forEach((button) => {
+  button.addEventListener('click', function (e) {
+    if (e.currentTarget.classList.contains('increase')) {
+      counterValue++
+      counterSpan.textContent = counterValue
+    } else if (e.currentTarget.classList.contains('decrease')) {
+      counterValue -= 1
+      counterSpan.textContent = counterValue
+    } else {
+      counterValue = 0
+      counterSpan.textContent = 0
+    }
+    if (counterValue > 0) counterSpan.style.color = 'green'
+    if (counterValue < 0) counterSpan.style.color = 'red'
+    if (counterValue === 0) counterSpan.style.color = '#222'
+  })
 })
 
-increaseButton.addEventListener('click', function (e) {
-  counterValue += 1
-  counterSpan.textContent = counterValue
-  if (counterValue > 0) counterSpan.style.color = 'green'
-  if (counterValue === 0) counterSpan.style.color = '#222'
-})
+// = focusing each element
+// decreaseButton.addEventListener('click', function (e) {
+//   counterValue -= 1
+//   counterSpan.textContent = counterValue
+//   if (counterValue < 0) counterSpan.style.color = 'red'
+//   if (counterValue === 0) counterSpan.style.color = '#222'
+// })
+// resetButton.addEventListener('click', function (e) {
+//   counterValue = 0
+//   counterSpan.textContent = counterValue
+//   if (counterValue === 0) counterSpan.style.color = '#222'
+// })
+
+// increaseButton.addEventListener('click', function (e) {
+//   counterValue += 1
+//   counterSpan.textContent = counterValue
+//   if (counterValue > 0) counterSpan.style.color = 'green'
+//   if (counterValue === 0) counterSpan.style.color = '#222'
+// })
